74b3d14caa776f255486c296bc68effd6930df87
nittosantos <nitto_kun@hotmail.com>
2022-06-22 23:27:50 -0300

    adicionando apollo e graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 function App() {
   return (
-    <div>
-      <h1>Hello ignite</h1>
-    </div>
+    <h1 className="p-4">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero expedita
+      in sint porro. Vero natus odio rem cumque tempore fuga harum, excepturi
+      voluptates nesciunt assumenda et nihil animi iure nisi.
+    </h1>
   );
 }
 
